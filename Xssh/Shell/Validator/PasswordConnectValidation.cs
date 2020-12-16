0c1bb31d7f9454f53244b064a865d3eecfe670c5
Xis <xishang7707@163.com>
2020-12-17 06:48:56 +0800

    1

---
+++ Xssh/Shell/Validator/PasswordConnectValidation.cs
@@ -0,0 +1,25 @@
+﻿using Core.Commands.Models;
+using Domain.Shell.Commands;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xssh.Shell.Validator
+{
+    public class PasswordConnectValidation : ShellValidation<PasswordOpenShellCommand, CommandDataResult<string>>
+    {
+        public override ValidationResult IsValid(PasswordOpenShellCommand command)
+        {
+            return Validate(new Domain.Models.SV_Server
+            {
+                Host = command.PasswordOpenShell.Host,
+                Port = command.PasswordOpenShell.Port,
+                User = command.PasswordOpenShell.User,
+                Password = command.PasswordOpenShell.Password,
+            });
+        }
+    }
+}
---
+++ Xssh/Shell/Validator/ShellValidation.cs
@@ -0,0 +1,17 @@
+﻿using Core.Commands.Models;
+using Core.Validator;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xssh.Shell.Validator
+{
+    public abstract class ShellValidation<TCommand, TResult> : CommandValidator<TCommand, TResult, SV_Server>
+       where TCommand : Command<TResult>
+       where TResult : CommandResult, new()
+    {
+    }
+}
